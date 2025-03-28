1f3d884febb0086c0d5c977a885e31872cbebfdd
Gianina Azcurra <gianinazcurra@hotmail.com>
2025-03-28 19:22:46 -0300

    primer commit segundo ejercicio

---
+++ practicaGit2.py
@@ -0,0 +1,4 @@
+nombre=input("Ingrese su nombre")
+apellido=input("Ingrese su apellido")
+edad=input("Ingrese su edad")
+print(f"Hola, mi nombre es {nombre} y mi apellido {apellido} y mi edad es {edad}")
